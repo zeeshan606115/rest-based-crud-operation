46f85961e201cf9196ffde2a10505136ba6df95e
unknown <zeeshan.husain54@gmail.com>
2020-07-18 00:35:17 +0530

    Second commit

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
-from .views import articleList, article_detail, article_List, article_detail_api
+from django.urls import path, include
+from .views import articleList, article_detail, article_List, \
+    article_detail_api, ArticleApiView, ArticleDetails, \
+    GenericAPIView, ArticleViewSet, ArticleGenericViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+router.register('article', ArticleGenericViewSet, basename='article')
 urlpatterns = [
+    path('viewset/',include(router.urls) ),
+    path('genericviewset/',include(router.urls) ),
+    path('viewset/<int:pk>/',include(router.urls) ),
     path('article', articleList),
     path('article_list', article_List),
+    path('article_list_class', ArticleApiView.as_view()),
+    path('generic/<int:id>/', GenericAPIView.as_view()),
     path('article_detail/<int:pk>/', article_detail),
     path('article_detail_api/<int:pk>/', article_detail_api),
+    path('article_detail_class/<int:id>/', ArticleDetails.as_view()),
 ]
